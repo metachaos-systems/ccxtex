a10e250f79ec91fe63d52c088a8eb54bb93688a9
ontofractal <ontofractal@protonmail.com>
2018-05-21 14:01:03 +0100

    add capability to fetch markets for given exchange

--- lib/ccxtpy.ex
+++ lib/ccxtpy.ex
@@ -7,6 +7,13 @@ defmodule Ccxtpy do
     Python.call(py, "ccxt_port", "exchanges", [])
   end
 
+  def fetch_markets_for_exchange(exchange) do
+    {:ok, py} = Python.start(python_path: Path.expand("priv/python"))
+
+    res = Python.call(py, "ccxt_port", "fetch_markets_for_exchange", [exchange])
+    Poison.Parser.parse!(res)
+  end
+
   @moduledoc """
   Documentation for Ccxtpy.
   """
--- priv/python/ccxt_port.py
+++ priv/python/ccxt_port.py
@@ -1,6 +1,16 @@
 import time
 import ccxt  
+import json
 
 def exchanges(): 
-    return ccxt.exchanges
+    exchanges = {}  
 
+    for id in ccxt.exchanges:
+        exchange = getattr(ccxt, id)
+        exchanges[id] = exchange()
+    return exchanges
+
+
+def fetch_markets_for_exchange(exchange_id):
+    res = exchanges()[exchange_id].load_markets()
+    return json.dumps(res)
