f34cd5d39f591279ed4eaca5bbc88fb7ef91d4a3
ontofractal <ontofractal@protonmail.com>
2018-05-21 15:10:27 +0100

    add ohlcvs parsing

--- lib/ccxtpy.ex
+++ lib/ccxtpy.ex
@@ -1,5 +1,6 @@
 defmodule Ccxtpy do
   use Export.Python
+
   @moduledoc """
   Documentation for Ccxtpy.
   """
@@ -28,11 +29,37 @@ defmodule Ccxtpy do
   end
 
   def fetch_ohlcvs(exchange, symbol, timeframe, since, limit) do
+    [base, quote] = String.split(symbol, "/")
+
+    since =
+      if since do
+        since
+        |> DateTime.from_naive!("Etc/UTC")
+        |> DateTime.to_unix(:millisecond)
+      else
+        since
+      end
+
     {:ok, py} = Python.start(python_path: Path.expand("priv/python"))
-    since = since |> DateTime.from_naive!("Etc/UTC") |> DateTime.to_unix(:millisecond)
 
     res = Python.call(py, "ccxt_port", "fetch_ohlcv", [exchange, symbol, timeframe, since, limit])
-    Poison.Parser.parse!(res)
+
+    res
+    |> Poison.Parser.parse!()
+    |> parse_ohlcvs()
+    |> Enum.map(&Map.merge(&1, %{base: base, quote: quote}))
   end
 
+  def parse_ohlcvs(raw_ohlcvs) do
+    for [unix_time_ms, open, high, low, close, volume] <- raw_ohlcvs do
+      %{
+        timestamp: unix_time_ms |> DateTime.from_unix!(:millisecond) |> DateTime.to_naive(),
+        open: open,
+        high: high,
+        low: low,
+        close: close,
+        base_volume: volume
+      }
+    end
+  end
 end
--- test/ccxtpy_test.exs
+++ test/ccxtpy_test.exs
@@ -3,16 +3,13 @@ defmodule CcxtpyTest do
   use ExUnit.Case
   doctest Ccxtpy
 
-  test "greets the world" do
-    assert Ccxtpy.hello() == :world
-  end
-
   test "fetches ohlcvs from bitfinex2" do
     exchange = "bitfinex2"
     symbol = "ETH/USDT"
     timeframe = "1h"
     since = ~N[2018-01-01T00:00:00]
     limit = 1000
-    assert fetch_ohlcvs(exchange, symbol, timeframe, since, limit) == []
+    ohlcvs = fetch_ohlcvs(exchange, symbol, timeframe, since, limit)
+    assert %{base: _, high: _, base_volume: _} = hd(ohlcvs)
   end
 end
