8a102aae5bff800fff7f726c10d89b2ddfd4ff3c
ontofractal <ontofractal@protonmail.com>
2018-05-23 15:07:08 +0100

    add poloniex test

--- test/ccxtpy_test.exs
+++ test/ccxtpy_test.exs
@@ -15,6 +15,17 @@ defmodule CcxtpyTest do
     assert %{base: _, high: _, base_volume: _} = hd(ohlcvs)
   end
 
+  test "fetches ohlcvs from poloniex" do
+    exchange = "poloniex"
+    symbol = "ETH/USDT"
+    timeframe = "1h"
+    since = ~N[2018-01-01T00:00:00]
+    limit = 1000
+    ohlcvs = fetch_ohlcvs(@pid, exchange, symbol, timeframe, since, limit)
+    assert %{base: _, high: _, base_volume: _} = hd(ohlcvs)
+  end
+
+
   test "fetches exchanges list" do
     exchanges = exchanges(@pid)
     assert %{has: _, id: _, timeout: _} = exchanges["bitfinex2"]
