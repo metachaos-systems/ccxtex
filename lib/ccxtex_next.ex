fce77cb1d6d3abad4942bf57f05d27777c2d5e3c
ontofractal <ontofractal@protonmail.com>
2018-10-10 13:52:38 +0100

    add fetch_ohlcvs fn, specs and test

--- lib/ccxtex_next.ex
+++ lib/ccxtex_next.ex
@@ -1,6 +1,7 @@
 defmodule Ccxtex.Next do
-  
-  @spec exchanges() :: [String.t]
+  alias Ccxtex.OHLCVS.Opts
+
+  @spec exchanges() :: [String.t()]
   def exchanges() do
     js_fn = {"main.js", :exchanges}
 
@@ -11,4 +12,25 @@ defmodule Ccxtex.Next do
     end
   end
 
+  @spec fetch_ohlcvs(OHLCVS.Opts.t()) :: {:ok, any} | {:error, String.t()}
+  def fetch_ohlcvs(%Ccxtex.OHLCVS.Opts{} = opts) do
+    js_fn = {"main.js", :fetchOhlcvs}
+
+    since_unix = if opts.since do
+      opts.since
+      |> DateTime.from_naive!("Etc/UTC")
+      |> DateTime.to_unix(:millisecond)
+    end
+
+    opts =
+      opts
+      |> Map.from_struct()
+      |> Map.put(:since, since_unix)
+
+    with {:ok, ohlcvs} <- NodeJS.call(js_fn, [opts]) do
+      {:ok, ohlcvs}
+    else
+      err_tup -> err_tup
+    end
+  end
 end
--- test/ccxt_next_test.exs
+++ test/ccxt_next_test.exs
@@ -5,7 +5,37 @@ defmodule Ccxtex.NextTest do
 
   test "returns exchanges list" do
     {:ok, exchanges} = exchanges()
-    assert ("poloniex" in exchanges) and ("bitfinex" in exchanges)
+    assert "poloniex" in exchanges and "bitfinex" in exchanges
+  end
+
+  test "fetches ohlcvs from poloniex" do
+    opts =
+      Ccxtex.OHLCVS.Opts.make!(%{
+        exchange: "poloniex",
+        base: "ETH",
+        quote: "USDT",
+        timeframe: "1h",
+        since: ~N[2018-01-01T00:00:00],
+        limit: 100
+      })
+
+    {:ok, ohlcvs} = fetch_ohlcvs(opts)
+    assert length(hd(ohlcvs)) == 6
+  end
+
+  test "fetches ohlcvs from bitfinex2" do
+    opts =
+      Ccxtex.OHLCVS.Opts.make!(%{
+        exchange: "bitfinex2",
+        base: "ETH",
+        quote: "USDT",
+        timeframe: "1h",
+        since: ~N[2018-01-01T00:00:00],
+        limit: 100
+      })
+
+    {:ok, ohlcvs} = fetch_ohlcvs(opts)
+    assert length(hd(ohlcvs)) == 6
   end
 
 end
