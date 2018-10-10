93d3b29a1c02558427c8b57f7e9e008d427ca083
ontofractal <ontofractal@protonmail.com>
2018-10-10 20:09:22 +0100

    add Utils module

---
+++ lib/ccxtex/utils.ex
@@ -0,0 +1,23 @@
+defmodule Ccxtex.Utils do
+  def parse_ohlcvs(raw_ohlcvs) do
+    for [unix_time_ms, open, high, low, close, volume] <- raw_ohlcvs do
+      %{
+        timestamp: unix_time_ms |> DateTime.from_unix!(:millisecond) |> DateTime.to_naive(),
+        open: parse_float(open),
+        high: parse_float(high),
+        low: parse_float(low),
+        close: parse_float(close),
+        base_volume: parse_float(volume)
+      }
+    end
+  end
+
+  defp parse_float(term) when is_binary(term) do
+    {float, _} = Float.parse(term)
+    float
+  end
+
+  defp parse_float(term) when is_float(term), do: term
+  defp parse_float(term) when is_integer(term), do: term
+  defp parse_float(nil), do: nil
+end
