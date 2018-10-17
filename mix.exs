47872f053ada8e2321b8d37253a3695b230d05d9
ontofractal <ontofractal@protonmail.com>
2018-10-17 16:31:35 +0100

    version bump

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Ccxtex.MixProject do
   def project do
     [
       app: :ccxtex,
-      version: "0.2.0",
+      version: "0.3.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
