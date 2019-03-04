e0ce722c25b05694b714da54956ef4245d71116c
ontofractal <ontofractal@protonmail.com>
2019-03-04 21:00:53 +0200

    remove ex_construct functions from tests

--- test/ccxt_test.exs
+++ test/ccxt_test.exs
@@ -11,14 +11,14 @@ defmodule Ccxtex.Test do
 
   test "fetches ohlcvs from poloniex" do
     opts =
-      Ccxtex.OHLCVS.Opts.make!(%{
-        exchange: "poloniex",
+      %Ccxtex.OHLCVS.Opts{
+        exchange: "binance",
         base: "ETH",
         quote: "USDT",
-        timeframe: "1h",
+        timeframe: "1m",
         since: ~N[2018-01-01T00:00:00],
-        limit: 100
-      })
+        limit: 1000
+      }
 
     {:ok, ohlcvs} = fetch_ohlcvs(opts)
     assert %OHLCV{open: _, close: _, high: _, low: _, base_volume: _, timestamp: _} = hd(ohlcvs)
@@ -26,17 +26,17 @@ defmodule Ccxtex.Test do
 
   test "fetches ohlcvs from bitfinex2" do
     opts =
-      Ccxtex.OHLCVS.Opts.make!(%{
+      %Ccxtex.OHLCVS.Opts{
         exchange: "bitfinex2",
         base: "ETH",
-        quote: "USDT",
+        quote: "USD",
         timeframe: "1h",
         since: ~N[2018-01-01T00:00:00],
         limit: 100
-      })
+      }
 
     {:ok, ohlcvs} = fetch_ohlcvs(opts)
-    assert %OHLCV{open: _, close: _, high: _, low: _, base_volume: _, timestamp: _} = hd(ohlcvs)
+    assert %OHLCV{open: _, close: _, high: _, low: _, base_volume: _, timestamp: _} = hd(tl ohlcvs)
   end
 
   test "fetch bitfinex ticker" do
@@ -59,7 +59,7 @@ defmodule Ccxtex.Test do
   end
 
   test "fetch markets for bitstamp" do
-    exchange = "bitstamp"
+    exchange = "binance"
     {:ok, markets} = fetch_markets(exchange)
 
     assert %Market{
