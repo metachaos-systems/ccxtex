742b85dacf6ffb9c2423d8ed167a0496230e0df8
ontofractal <ontofractal@protonmail.com>
2019-04-01 13:47:46 +0100

    automate js deps management

--- build_ccxtex_js.sh
+++ build_ccxtex_js.sh
@@ -1,3 +1,6 @@
 #! /bin/bash
 
+cd ./js/source && \
+npm upgrade && \
+cd ../.. && \
 webpack-cli ./js/source/main.js  -o ./priv/js/dist/main.js -p --output-library ccxtex --output-library-target commonjs --target node
